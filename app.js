68304b27f649403f1af7d7dc1c5d8a7d2c92707c
HasanbasriUysal <48217@edu.raseko.fi>
2023-08-29 15:11:34 +0300

    Added main program app.js

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // WEB SERVER FOR ELECTRICITY USAGE PLANNIG: WEB PAGES AND AN API 
 //===============================================================
+
+// LIBRARIES AND MODULES
+//----------------------
+
+// Use Express as web engine
+const express = require("express");
+// Use Express Handlebars as template engine 
+const {engine} = require("express-handlebars");
+
+// EXPRESS APPLICATION SETTINGS
+// ----------------------------
+
+// Create the server
+const app = express();
+const PORT = process.env.PORT || 8080
+
+// Set folder paths: public is for assets and views for pages 
+app.use(express.static("public"));
+app.set("views", "./views");
+
+//Engine settings
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+// URL ROUTES
+// ----------
+
+
+//START THE LISTENER
+app.listen(PORT);
+console.log("Server started and it will listen TCP port",PORT);
