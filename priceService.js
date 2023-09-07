14f9d328122a0bd497b243874947d6bd9d35dce2
HasanbasriUysal <48217@edu.raseko.fi>
2023-09-07 14:10:00 +0300

    added

---
+++ priceService.js
@@ -0,0 +1,29 @@
+//Set the URL for API endpoint: latest prices at 14.00 EET
+const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json';
+ 
+/*Create a promise for reading data from the API in the background
+and return it as json when ready */
+async function fetchLatestPriceData() {
+  const response = await fetch(LATEST_PRICES_ENDPOINT);
+  const json = await response.json()
+  return json;
+}
+ 
+//Get price for a given hour inteval 
+function getPriceForDate(date, prices) {
+  const matchingPriceEntry = prices.find(
+    (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
+  );
+ 
+  //If not found raise an error 
+  if (!matchingPriceEntry) {
+    throw 'Price for the requested date is missing';
+  }
+ 
+  return matchingPriceEntry.price;
+} 
+
+//When promise has been fullfilled log results or the error 
+fetchLatestPriceData()
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err))
