efdfc560ebc7ac4bebc2954662053b5468009c8c
HasanbasriUysal <48217@edu.raseko.fi>
2023-10-03 10:37:50 +0300

    added

--- logger.js
+++ logger.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
 
+const timestamp = new Date();
+const isoTimeStamp = timestamp.toISOString();
+let operation = 'Data Fetch operation';
+let status = 'processed successfully';
+let entry = operation + ' ' +status + '@' + isoTimeStamp + '\n';
+
+console.log(entry)
+
+fs.appendFile('.dataOperations.log',entry,(err) => {
+    if (err){
+        console.log(err)
+    }
+})
+
+//TODO: Create a function to do this ie.create log message and timestamp
--- microservice.js
+++ microservice.js
@@ -15,7 +15,7 @@ const getPrices = require('./getNewPrices');
 
 // Create a new pool for Postgres connections
 const pool = new Pool({
-  user: 'postgres', // In production allways create a new user for the app
+  user: 'postgres', // In production always create a new user for the app
   password: 'Q2werty',
   host: 'localhost', // Or localhost or 127.0.0.1 if in the same computer
   database: 'smarthome',
@@ -25,41 +25,46 @@ const pool = new Pool({
 // GET, PROCESS AND SAVE DATA
 // --------------------------
 
-// Use a date variable to keep track of successfull data retrievals
-let lastFethcedDate = '1.1.2023'; // Initial value, in production use settings file
+// Use a date variable to keep track of successful data retrievals
+let lastFetchedDate = '1.1.2023'; // Initial value, in production use settings file
 
 // Try to run an operation in 5 minute intervals from 3 to 4 PM
-cron.schedule('*/5 15 * * *', () => {
+cron.schedule('*/5 11 * * *', () => {
   try {
     let timestamp = new Date(); // Get the current timestamp
-    let dateStr = timestamp.toLocaleDateString(); // Take datepart of the timestamp
+    let dateStr = timestamp.toLocaleDateString(); // Take date part of the timestamp
 
-    // If the date of last sucessfull fetch is not the current day, fetch data
-    if (lastFethcedDate != dateStr) {
-      console.log('Started fething price data ');
+    // If the date of last successful fetch is not the current day, fetch data
+    if (lastFetchedDate != dateStr) {
+      console.log('Started fetching price data ');
+      // TODO: Add this to the log file
       getPrices.fetchLatestPriceData().then((json) => {
 
-        //loop trough prices data and pick starDate and price elements
+        // Loop through prices data and pick startDate and price elements
         json.prices.forEach(async (element) => {
           let values = [element.startDate, element.price];
 
           // Build a SQL clauset to insert values into table
           const sqlClause = 'INSERT INTO public.hourly_price VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *'; 
-          // Function for running SQL operations asyncroneously
+          // Function for running SQL operations asyncronously
           const runQuery = async () => {
             let resultset = await pool.query(sqlClause, values);
             return resultset;
           }
-          // Call queryfunction and echo results to console
+          // Call query function and echo results to console
           runQuery().then((resultset) => console.log(resultset.rows[0]))
+          // TODO: add this entry to the log file
         });
       });
-      lastFethcedDate = dateStr; // Set fetch date to current date
-      console.log('Fethed at', lastFethcedDate)
+      lastFetchedDate = dateStr; // Set fetch date to current date
+      console.log('Fetched at', lastFetchedDate)
+      // TODO: add this entry to the log file
     } else {
       console.log('Data has been successfully retrieved earlier today');
+      // TODO: add this entry to the log file
     }
   } catch (error) {
-    console.log('An error occurred, trying again in 5 minutes until 4 PM');
+    console.log('An error() occurred, trying again in 5 minutes until 4 PM');
+    // TODO: add this entry and the error message to the log file
   }
 });
