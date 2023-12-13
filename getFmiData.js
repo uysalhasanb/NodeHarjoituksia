e415f0aaa0296c768c410c48890e60237618e3a3
HasanbasriUysal <48217@edu.raseko.fi>
2023-12-13 13:03:39 +0200

    added

---
+++ getFmiData.js
@@ -0,0 +1,37 @@
+// A MODULE FOR FETCHING FMI OBSERVATIONS AND FORECASTS
+// ====================================================
+
+// LIBRARIES AND MODULES
+// ---------------------
+
+// FUNCTIONS TO GET DATA FROM FMI
+// ------------------------------
+
+// TODO: Create functions for skeletons
+/** 
+* Fetches FMI observation data from FMI WFS API
+* @summary Get weather observations from a given place
+* @param {str} apicall - An URL for the WFS service and predefined query
+* @param {str} place - Name of the monitoring station
+* @param {str} parameters - Value of interest or comma separated list of values
+* @return {str} Results in GML (XML) astring
+*/
+
+const getObservations = (apicall, place, parameters) => {
+    let results = ''
+    return results
+}
+
+/** 
+* Fetches FMI observation data from FMI WFS API
+* @summary Get weather observations from a given place
+* @param {str} apicall - An URL for the WFS service and predefined query
+* @param {str} place - Name of the monitoring station
+* @param {str} parameters - Value of interest or comma separated list of values
+* @return {str} Results in GML (XML) astring
+*/
+
+const getForecasts = (apicall, place, parameters) => {
+    let results = ''
+    return results
+}
